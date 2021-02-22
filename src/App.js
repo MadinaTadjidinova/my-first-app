a4cc106f0c5a319a935537e2d7524d66deb36aaa
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-02-22 15:56:30 +0600

    Add function Person

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Person(props) {
+  let smokes = <strong>Ne kurit</strong>
+  if (props.smokes) {
+    smokes = <strong>Kurit</strong>
+  }
+ 
+ return <div className="person">
+   <h1>{ props.name }</h1>
+   <div className="age">{ props.age } years old</div>
+   <p className="hobby">{ props.children }</p>
+   <strong>{ smokes }</strong>
+ </div>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <Person name="Elon Musk" age={41}>Listens to <strong>music</strong></Person>
+         <Person name="Jeff Besos" age={55} smokes={true} />
+         <Person name="Bill Gates" age={56} hobbies={["Singing", "Dancing"]}>Travels</Person>
+
+         <Person name="Madina" age={18}>Play in <strong>baleiboll</strong></Person>
+         <Person name="Elaman Imashov" age={35} smokes={true} />
+         <Person name="Pak Shin Her" age={31} hobbies={["Singing", "Watching moves"]}>Acter</Person>
     </div>
   );
 }
