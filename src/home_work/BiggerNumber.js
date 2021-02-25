ae1d7aca4d56705b227c8683c92f32c2bc2969c6
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-02-25 21:19:41 +0600

    function BiggerNumber

---
+++ src/home_work/BiggerNumber.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function BiggerNumber(props){
+    let a = props.a
+    let b = props.b
+        if (a < b){
+            return <div>Bigger numer is {props.b }</div>
+        }
+        else {
+            return <div>Bigger number is {props.a}</div>
+        }
+    }
+    
+
+export default BiggerNumber;
