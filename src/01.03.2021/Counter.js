4c0842f0b90dcab00bea6e254725ba5c8f996e08
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-03-03 14:04:18 +0600

    Changes some properrtys

--- src/01.03.2021/Counter.js
+++ src/01.03.2021/Counter.js
@@ -8,7 +8,7 @@ function Counter() {
             <h1>count: {number}</h1>
             <button onClick={() => setNumber(number + 1)}>Increase</button>
 
-            <button onClick={() => setNumber(number - 1)}>decrease</button>
+            <button onClick={() => setNumber(number - 1)}>Decrease</button>
         </div>
     )
 }
