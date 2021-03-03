81afec86d77a5b24c71a8710b9fd7389d7835029
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-03-03 12:29:04 +0600

    DO list but can't add b

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 import React from "react";
 import Switch from "./01.03.2021/Switch";
@@ -5,10 +6,10 @@ import Counter from "./01.03.2021/Counter";
 import Name from "./01.03.2021/Name";
 import Text from "./01.03.2021/Text";
 
-import TodoList from "./home_work/TodoList";
-
+import Form from "./home_work/Form";
 
 function App() {
+  const [todos, setTodos] = useState([]);
 
   return (
     <div className="App">
@@ -18,10 +19,27 @@ function App() {
       <hr />
       <Text />
       <hr />
-      <TodoList />
-
+  
+      <Form
+      onSubmit={text => setTodos([{ text, complete: false }, ...todos])}
+    />
+    <div>
+      {todos.map(({ text }) => (
+        <ul>
+          <li
+          key={text}
+        >
+          {text}
+          </li>
+          </ul>
+      ))}
     </div>
-  )
-}
+   
+  </div>
+);
+};
+  
 
 export default App;
+
+
