76e011977934a001865fb42ce4c6e5a02b31f109
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-03-04 23:57:11 +0600

    Delate some propertys

--- src/home_work/Reverse.js
+++ src/home_work/Reverse.js
@@ -4,14 +4,14 @@ function Reverse({ value }) {
     const [text, setText] = useState(value ?? "");
 
 
-    let reversedText = text.split("").reverse().join("");
+    let reverseText = text.split("").reverse().join("");
     return (
         <div className="Reverse">
             <input
                 type="text"
                 value={text}
                 onInput={(event) => setText(event.target.value)} />
-            <span>{reversedText}</span>
+            <span>{reverseText}</span>
         </div>
     );
 }
