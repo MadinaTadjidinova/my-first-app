601d5f7c1d89f4e136e019c4e8c1dd3b86c4467f
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-03-05 00:01:53 +0600

    Changes some propertys

--- src/home_work/Rotate.js
+++ src/home_work/Rotate.js
@@ -7,9 +7,9 @@ function Rotate({ value }) {
       <div
         style={{
           backgroundColor: "red",
-          width: 100 + "px",
-          height: 100 + "px",
-          margin: 10 + "px",
+          width: 80 + "px",
+          height: 80 + "px",
+          margin: 30 + "px",
           transform: `rotate(${rotate + "deg"})`,
         }}
       ></div>
@@ -17,7 +17,7 @@ function Rotate({ value }) {
         type="range"
         min={0}
         max={360}
-        value={number}
+        value={rotate}
         onInput={(event) => setRotate(event.target.value)}
       />
     </div>
