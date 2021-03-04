14f266db52c98ca715214f2b3594b0ac698715f1
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-03-04 16:34:26 +0600

    DO HOME WORKS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 // import { useState } from "react";
 
-import React from "react";
+// import React from "react";
 // import Switch from "./01.03.2021/Switch";
 // import Counter from "./01.03.2021/Counter";
 // import Name from "./01.03.2021/Name";
-// import Text from "./01.03.2021/Text";
+import Text from "./01.03.2021/Text";
 
 // import Form from "./home_work/Form";
 // import List from "./01.03.2021/List";
 
-import InputLength from "./03.03.2021/InputLength";
-import Color from "./03.03.2021/Color";
-import InputRange from "./03.03.2021/InputRange";
-import Dollar from "./03.03.2021/Dollar";
-import Temp from "./03.03.2021/Temp";
-import FontSize from "./03.03.2021/FontSize";
-import InputColor from "./03.03.2021/InputColor";
-import ColorSliders from "./03.03.2021/ColorSliders";
+// import InputLength from "./03.03.2021/InputLength";
+// import Color from "./03.03.2021/Color";
+// import InputRange from "./03.03.2021/InputRange";
+// import Dollar from "./03.03.2021/Dollar";
+// import Temp from "./03.03.2021/Temp";
+// import FontSize from "./03.03.2021/FontSize";
+// import InputColor from "./03.03.2021/InputColor";
+// import ColorSliders from "./03.03.2021/ColorSliders";
+
+import Rotate from "./home_work/Rotate";
+import Reverse from "./home_work/Reverse";
 
 function App() {
   // const [todos, setTodos] = useState([]);
@@ -39,14 +42,14 @@ function App() {
             <li>{text}</li>
           </ul>
         ))}
-      </div>
+      </div> */}
+      <Text />
 
-
-      <List/>
+      {/* <List/>
       <hr />
       <hr />
       <hr /> */}
-      <h3 >Example 1</h3>
+      {/* <h3 >Example 1</h3>
       <InputLength value="test" />
 
       <h3>Example 2</h3>
@@ -68,7 +71,13 @@ function App() {
       <InputColor />
 
       <h3>Example 8</h3>
-      <ColorSliders min={0} max={255}/>
+      <ColorSliders min={0} max={255}/> */}
+
+      <h3>home_work</h3>
+      <Rotate value={0} />
+
+      <h3>home_work2</h3>
+      <Reverse />
     </div>
   );
 }
