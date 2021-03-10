184c764b75f41446eabd7461b50d21a6dda562ba
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-03-10 14:33:05 +0600

    Created new files

---
+++ src/10.03.2021/Person/Person.js
@@ -0,0 +1,11 @@
+import classes from "./Person.module.css";
+
+const Person = ({ name, age, children }) => {
+    return (<div className={classes.Person}>
+        <h2>{ name }</h2>
+        <h4 className={classes.age}>{ age } years old</h4>
+        <h5>{ children }</h5>
+      <span className="highlight">Hello</span>
+    </div>);
+}
+export default Person;
