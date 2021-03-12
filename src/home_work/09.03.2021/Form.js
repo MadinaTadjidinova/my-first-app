55505cf4aee6c2556fa3f86722da5775d63f2f30
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-03-12 15:40:52 +0600

    Created new component

---
+++ src/home_work/09.03.2021/Form.js
@@ -0,0 +1,13 @@
+import classes from "./Form.module.js"
+
+const Form = ({children}) => {
+    return(
+        <div>
+            <form>
+                <h2>Login</h2>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
