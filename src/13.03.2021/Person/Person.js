5b0d9814bab1240e9d8cf616c5bbd62cd6197c96
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-03-13 22:43:01 +0600

    Add position for divs

--- src/13.03.2021/Person/Person.js
+++ src/13.03.2021/Person/Person.js
@@ -1,13 +1,16 @@
 import classes from "./Person.module.css";
 
-const Person = ({ name, role, wpm, commits, img }) => {
+const Person = ({ name , role , wpm , commits , img }) => {
     return (
         <div className={classes.Person}>
+            <div className={classes.alldiv}>
             <div className={classes.name}>{name}</div>
             <div className={classes.role}>{role}</div>
             <div className={classes.wpm}>{wpm}WPM</div>
             <div className={classes.commits}>{commits} commits</div>
-            <img className={classes.img} src={img} alt={"img"}/>
+            </div>
+        
+            <div className={classes.divimg}><img className={classes.img} src={img} alt={"img"}/></div>
         </div>
     );
 }
