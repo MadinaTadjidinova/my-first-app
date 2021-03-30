101916d3eddc0783a3ab4740413a7c606cab6e61
Madina Tadjidinova <madina.tadjidinova@gmail.com>
2021-03-31 03:47:24 +0600

    Add Component Properties

--- src/App.js
+++ src/App.js
@@ -204,61 +204,66 @@
 
 // export default App;
 
+import "./App.css";
 import { useState } from "react";
 import Countdown from "./Math/Countdown/Countdown";
 import Header from "./Math/Header/Header";
 import Logo from "./Math/Logo/Logo";
 const App = () => {
-  const [a, setA] = useState(randomNumber(50));
-  const [b, setB] = useState(randomNumber(50));
-  const [c, setC] = useState(randomNumber(50));
+  // const [a, setA] = useState(randomNumber(50));
+  // const [b, setB] = useState(randomNumber(50));
+  // const [c, setC] = useState(randomNumber(50));
 
-  const [a1, setA1] = useState(randomNumber(50));
-  const [b1, setB1] = useState(randomNumber(50));
+  // const [a1, setA1] = useState(randomNumber(50));
+  // const [b1, setB1] = useState(randomNumber(50));
 
 
-  const [answer, setAnswer] = useState("?");
-  const [answer1, setAnswer1] = useState("?");
-  const [countdown, setCountdown] = useState(20);
-  function randomNumber(limit) {
-    return Math.round(Math.random() * limit);
-  }
-  function checkAnswer() {
-    const result = a + b - c;
-    if (result == answer) {
-      setA(randomNumber(50));
-      setB(randomNumber(50));
-      setC(randomNumber(50));
+  // const [answer, setAnswer] = useState("?");
+  // const [answer1, setAnswer1] = useState("?");
+  // const [countdown, setCountdown] = useState(20);
+  // function randomNumber(limit) {
+  //   return Math.round(Math.random() * limit);
+  // }
+  // function checkAnswer() {
+  //   const result = a + b - c;
+  //   if (result == answer) {
+  //     setA(randomNumber(50));
+  //     setB(randomNumber(50));
+  //     setC(randomNumber(50));
 
-      setAnswer("?");
-      setCountdown(20);
-    }
-         else {
-      alert("Wrong");
-    }
-    const result1 = a1 * b1;
-    if (result1 == answer1){
-      setA1(randomNumber(30));
-      setB1(randomNumber(30));
+  //     setAnswer("?");
+  //     setCountdown(20);
+  //   }
+  //        else {
+  //     alert("Wrong");
+  //   }
+  //   const result1 = a1 * b1;
+  //   if (result1 == answer1){
+  //     setA1(randomNumber(30));
+  //     setB1(randomNumber(30));
 
-      setAnswer1("?");
-      setCountdown(20);
-    }
-    else {
-      alert("Wrong1");
-    }
+  //     setAnswer1("?");
+  //     setCountdown(20);
+  //   }
+  //   else {
+  //     alert("Wrong1");
+  //   }
 
-  }
+  // }
   return (
     <div className="App">
 <Logo />
-<Header />
-      {a} + {b} - {c}= {answer} <br />
+{/* 
+  {a} + {b} - {c}= {answer} <br />
       {a1} * {b1} = {answer1}
       <Countdown countdown={countdown} setCountdown={setCountdown} />
       <input type="number" value={answer} onChange={({ target }) => setAnswer(target.value)} /> <br />
       <input type="number" value={answer1} onChange={({ target }) => setAnswer1(target.value)} />
-      <button onClick={() => checkAnswer()}>Ok</button>
+      <button onClick={() => checkAnswer()}>Ok</button> */}
+
+  
+<Header />
+     
     </div>
   );
 
--- src/Math/Header/Header.js
+++ src/Math/Header/Header.js
@@ -1,10 +1,11 @@
 import classes from "./Header.module.css";
-import BackToSchoolFixedNoText from "../../Image/BackToSchoolFixedNoText.svg"
+import BackToSchoolFixedNoText from "../../Image/BackToSchoolFixedNoText.svg";
+
 const Header = () => {
-    return ( 
-        <div className={classes.Header}>
- <img src={BackToSchoolFixedNoText} alt="blackboard" />;
-       </div>
- );
-}
+  return (
+    <div className={classes.Header}>
+      <img src={BackToSchoolFixedNoText} alt="blackboard" />;
+    </div>
+  );
+};
 export default Header;
